93af50752bbc71ad9d9cf1e264768eade02e6455
YuMeihua123 <90707953+YuMeihua123@users.noreply.github.com>
2022-08-09 12:03:31 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,9 +21,12 @@ st.dataframe(fruits_to_show)
 
 # New selection to display fruityvice response
 st.header('Fruityvice Fruit Advice!')
+fruit_choice = st.text_input('What fruit would you like information about?','Kiwi')
+st.write('The user entered ', fruit_choice)
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +  fruit_choice)
 # st.text(fruityvice_response.json()) # not show json format data in screen
 
 # write your own comment -what does the next line do? 
